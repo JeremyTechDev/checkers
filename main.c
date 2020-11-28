575ca469673049d718cdd0c68a7bf16a9a0e51ac
Jeremy Munoz Torres <jeremy2918@gmail.com>
2020-11-28 11:14:55 -0400

    Refactored main function

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 void runRound(Team, PieceList *);
 void printTurnInfo(Team, PieceList *, MoveList *);
+Piece getPieceToMove(PieceList *, Team);
 
 int main(int argc, char const *argv[])
 {
@@ -31,8 +32,58 @@ void runRound(Team team, PieceList *pieceList)
 
     while (1) // to get piece and moving spot
     {
-        Coord pieceChoice = getCoordsFromUser();
-        Piece *pieceToMove = getPieceAtPosition(pieceList, pieceChoice.x, pieceChoice.y);
+        Piece pieceToMove = getPieceToMove(pieceList, team);
+
+        MoveList *possibleMoves = getPossibleMoves(pieceList, pieceToMove);
+        MoveList *toHighlight = possibleMoves;
+        Move move = {pieceToMove.coord, -1};
+        insertMove(&toHighlight, move); // insert the selected piece to highlight
+
+        if (possibleMoves)
+        {
+            system("clear");
+            printTurnInfo(team, pieceList, toHighlight);
+            printColorText("These are your possible moves:\n", BLUE);
+            printTextCoord(possibleMoves);
+            printf(">>> ");
+
+            while (1) // to move
+            {
+                Coord moveChoice = getCoordsFromUser();
+                Move *isMoveValid = isMoveInList(possibleMoves, moveChoice.x, moveChoice.y);
+                if (isMoveValid)
+                {
+                    printf("??%d\n", isMoveValid->killedPieceId);
+                    Piece newPiece = {pieceToMove.id, {moveChoice.x, moveChoice.y}, team, 1};
+                    modifyPiece(&pieceList, pieceToMove.id, newPiece);
+
+                    if (isMoveValid->killedPieceId != -1)
+                    {
+                        Piece killedPiece = {isMoveValid->killedPieceId, {0, 0, regular}, white, 0};
+                        modifyPiece(&pieceList, isMoveValid->killedPieceId, killedPiece);
+                    }
+                    break;
+                }
+                else
+                    printColorText("That's not a possible move, try another one: \a", RED);
+            }
+            clear(0);
+            break;
+        }
+        else
+            printColorText("No possible moves for that piece, try another one: \a", RED);
+    }
+}
+
+Piece getPieceToMove(PieceList *pieceList, Team team)
+{
+    Coord pieceChoice;
+    Piece *pieceToMove;
+
+    while (1)
+    {
+        pieceChoice = getCoordsFromUser();
+        pieceToMove = getPieceAtPosition(pieceList, pieceChoice.x, pieceChoice.y);
 
         if (pieceToMove)
         {
@@ -42,44 +93,9 @@ void runRound(Team team, PieceList *pieceList)
                 continue;
             }
 
-            MoveList *possibleMoves = getPossibleMoves(pieceList, *pieceToMove);
-            MoveList *toHighlight = possibleMoves;
-            Move move = {pieceChoice, -1};
-            insertMove(&toHighlight, move); // insert the selected piece to highlight
-
-            if (possibleMoves)
-            {
-                system("clear");
-                printTurnInfo(team, pieceList, toHighlight);
-                printColorText("These are your possible moves:\n", BLUE);
-                printTextCoord(possibleMoves);
-                printf(">>> ");
-
-                while (1) // to move
-                {
-                    Coord moveChoice = getCoordsFromUser();
-                    Move *isMoveValid = isMoveInList(possibleMoves, moveChoice.x, moveChoice.y);
-                    if (isMoveValid)
-                    {
-                        printf("??%d\n", isMoveValid->killedPieceId);
-                        Piece newPiece = {pieceToMove->id, {moveChoice.x, moveChoice.y}, team, 1};
-                        modifyPiece(&pieceList, pieceToMove->id, newPiece);
-
-                        if (isMoveValid->killedPieceId != -1)
-                        {
-                            Piece killedPiece = {isMoveValid->killedPieceId, {0, 0, regular}, white, 0};
-                            modifyPiece(&pieceList, isMoveValid->killedPieceId, killedPiece);
-                        }
-                        break;
-                    }
-                    else
-                        printColorText("That's not a possible move, try another one: \a", RED);
-                }
-                clear(0);
-                break;
-            }
-            else
-                printColorText("No possible moves for that piece, try another one: \a", RED);
+            Piece pieceToReturn = {pieceToMove->id, pieceToMove->coord, pieceToMove->team, pieceToMove->isOnGame};
+            pieceToReturn.coord.colorCode = orange; // set color code to orange to highlight in the board
+            return pieceToReturn;
         }
         else
             printColorText("No piece at that coord, try another one: \a", RED);
